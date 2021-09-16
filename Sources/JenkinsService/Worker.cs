619e55e6330db86103c290213a419197abe28018
Павел Завьялов <zavjalov@gmail.com>
2021-09-16 18:16:06 +0300

    Cosmetics

--- Sources/JenkinsService/Worker.cs
+++ Sources/JenkinsService/Worker.cs
@@ -21,7 +21,7 @@ namespace JenkinsService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
--- Sources/RedmineService/Worker.cs
+++ Sources/RedmineService/Worker.cs
@@ -21,7 +21,7 @@ namespace RedmineService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
