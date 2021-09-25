19c147ab42a06e2e437a8e55b90004cf8445f750
Павел Завьялов <zavjalov@gmail.com>
2021-09-25 18:43:38 +0300

    Add messages for users list webadmin

---
+++ Sources/RabbitMessageCommunication/WebAdmin/RequestAllUsersMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace RabbitMessageCommunication.WebAdmin
+{
+    /// <summary> Request all users from mainbot </summary>
+    public class RequestAllUsersMessage : IRabbitMessage
+    {
+        public RequestAllUsersMessage(string systemEventId)
+        {
+            this.SystemEventId = systemEventId;
+        }
+
+        public string SystemEventId { get; set; }
+    }
+}
---
+++ Sources/RabbitMessageCommunication/WebAdmin/ResponseAllUsersMessage.cs
@@ -0,0 +1,42 @@
+﻿namespace RabbitMessageCommunication.WebAdmin
+{
+    /// <summary> Response to get-all-user-message </summary>
+    public class ResponseAllUsersMessage : IRabbitMessage
+    {
+        public ResponseAllUsersMessage(string systemEventId)
+        {
+            this.SystemEventId = systemEventId;
+        }
+
+        public string SystemEventId { get; set; }
+
+        public UserInfo[] AllUsersInfos { get; set; }
+
+        /// <summary> Single user info </summary>
+        public class UserInfo
+        {
+            /// <summary> Syntethic user id </summary>
+            public int UserId { get; set; }
+
+            /// <summary> User id in bot-system </summary>
+            public string? BotUserId { get; set; }
+
+            /// <summary> Is user activated? </summary>
+            public bool IsActivate { get; set; }
+
+            /// <summary> User name in bot-system </summary>
+            public string? BotUserName { get; set; }
+
+            /// <summary> Autogenerate name in bot-system </summary>
+            public string? SystemUserInfo { get; set; }
+
+            /// <summary> Jenkins name </summary>
+            public string? JenkinsUserName { get; set; }
+
+            /// <summary> Redmine full name </summary>
+            public string? RedmineUserName { get; set; }
+
+        }
+
+    }
+}
