df0e2cfa30cdc627eaceff17679cf7723d84713f
Павел Завьялов <zavjalov@gmail.com>
2021-09-30 00:26:02 +0300

    Add dataase configuration function

--- Sources/CommonConfigurationExtensions/ConfigurationServiceExtension.cs
+++ Sources/CommonConfigurationExtensions/ConfigurationServiceExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -67,5 +68,30 @@ namespace CommonInfrastructure
             initilizeAction?.Invoke(host);
             host.Run();
         }
+
+
+        public static void ConfigureDatabase<TContext>(this IServiceCollection services, string dbName, IConfiguration configuration)
+            where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(optionsBuilder =>
+            {
+                var postgreHost = configuration.GetValue<string?>("POSTGRE_HOST")
+                                  ?? throw new NotSupportedException("Postgre host is not initialized");
+                var postgrePort = configuration.GetValue<string?>("POSTGRE_PORT")
+                                  ?? throw new NotSupportedException("Postgre port is not initialized");
+                var postgreUser = configuration.GetValue<string?>("POSTGRE_USER")
+                                  ?? throw new NotSupportedException("Postgre port is not initialized");
+                var postgrePassword = configuration.GetValue<string?>("POSTGRE_PASSWORD")
+                                      ?? throw new NotSupportedException("Postgre port is not initialized");
+
+                Console.WriteLine($"PostgreConnection: {postgreHost}:{postgrePort}");
+                optionsBuilder.UseNpgsql($"Host={postgreHost};Port={postgrePort};Database={dbName};Username={postgreUser};Password={postgrePassword}")
+                    .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                    //.EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            });
+
+        }
+
     }
 }
