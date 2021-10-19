0eb054c8dae1dfc2d286109ba8a28991ef881977
Павел Завьялов <zavjalov@gmail.com>
2021-10-20 02:46:25 +0300

    Add build-service-job-types

---
+++ Sources/InfrastructureServices/EnumBuildServerJobs.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonInfrastructure
+{
+    /// <summary> BuildServer job types </summary>
+    public enum EnumBuildServerJobs
+    {
+        /// <summary> Undef </summary>
+        Undef,
+
+        /// <summary> Dump Job </summary>
+        Dump,
+
+        /// <summary> Build current version </summary>
+        Current,
+
+        /// <summary> Build rc version </summary>
+        Rc,
+
+        /// <summary> System job </summary>
+        System 
+    }
+}
